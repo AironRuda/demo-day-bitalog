12e6b0574ce11f7b275a0a195a04ddde7564ab84
AironRuda <airon.ruda@udea.edu.co>
2023-03-03 16:51:22 -0500

    feat: worker list

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,12 +1,43 @@
-import * as React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  selectedProject,
+  selectProject,
+} from "../context/selectedProjectSlice";
+import { getCurrentProject } from "../context/userSlice";
+import { Project } from "../model/projects.model";
 
 interface IAppProps {}
 
 const Team: React.FunctionComponent<IAppProps> = (props) => {
+  const currentProjectId = useSelector(selectedProject);
+  const currentProject = useSelector(
+    // se crea un state, con el tipo requerido para el "getCurrentPRoject"
+    (state: { user: { projects: Project[] } }) =>
+      getCurrentProject(state, currentProjectId)
+  );
+  const workerArray = currentProject?.workers;
+
+  useEffect(() => {
+    // console.log(selectedProject);
+    console.log("selected", currentProjectId);
+    console.log(currentProject);
+    console.log(workerArray);
+  }, [selectProject]);
   return (
-    <div>
-      <h1>Team</h1>
-    </div>
+    <main>
+      <h1>Lista de trabajadores</h1>
+      {!!!currentProjectId && !!!workerArray ? (
+        <h1>No se ha seleccionado un ptoyecto</h1>
+      ) : (
+        workerArray?.map((worker) => (
+          <li key={worker}>
+            <p>foto de trabajadaro</p>
+            <h1>Id del trabajador: {worker}</h1>
+          </li>
+        ))
+      )}
+    </main>
   );
 };
 
