11bfaac0988f4b5780b43a0178a77129a3dae9f3
Juan Esteban Villegas Arias <estebanari29@gmail.com>
2023-03-06 18:10:22 -0500

    feat: updated project status

--- src/components/projects/ProjectList.tsx
+++ src/components/projects/ProjectList.tsx
@@ -1,26 +1,49 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectProject } from '../../context/selectedProjectSlice';
 import { selectProjects } from '../../context/userSelectors';
+import { updateStatusProject } from '../../context/userSlice';
 
 const ProjectList: React.FunctionComponent = (props) => {
   const projects = useSelector(selectProjects);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    projects.forEach((project) => {
+      if (
+        (project.activities.every((activity) => activity.completed === true) &&
+          project.completed === false) ||
+        (project.activities.some((activity) => activity.completed === false) &&
+          project.completed === true)
+      ) {
+        dispatch(updateStatusProject(project.id));
+      }
+    });
+    console.log(projects);
+  }, [projects]);
 
   return (
     <div>
       <h1>Proyectos</h1>
       <ul>
         {projects.length ? (
-          projects.map((project, index) => (
-            <li
-              key={index}
-              onClick={() => {
-                dispatch(selectProject(project.id));
-              }}
-            >
-              {project.name}
-            </li>
-          ))
+          projects
+            .filter((item) => item)
+            .sort((a, b) => {
+              if (!a.completed) return -1;
+              if (a.completed) return 1;
+              return 0;
+            })
+            .map((project, index) => (
+              <li
+                key={index}
+                onClick={() => {
+                  dispatch(selectProject(project.id));
+                }}
+              >
+                {project.name}
+              </li>
+            ))
         ) : (
           <div>
             En el momento no hay proyectos disponibles, crea alguno o contacta a
