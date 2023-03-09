51fe8bdc54d846697df6c25aeadb709751001e1e
AironRuda <airon.ruda@udea.edu.co>
2023-03-09 16:52:44 -0500

    feat: alert on successful notification

--- src/components/projects/CreateProjects.tsx
+++ src/components/projects/CreateProjects.tsx
@@ -1,18 +1,18 @@
-import { getDocs } from 'firebase/firestore';
-import { Form, Formik, FormikHelpers } from 'formik';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Swal from 'sweetalert2';
-import { addProject } from '../../context/projectsSlice';
-import { searchWorkers } from '../../firebase/queries';
-import { handleCreateProject } from '../../handlers/handleProject';
-import { createProjectDTO } from '../../model/projects.model';
-import { CREATE_PROJECT_VALIDATION_SCHEMA } from '../../utilities/formValidations';
-import SelectFormik from '../form/SelectFormik';
-import TextFieldFormik from '../form/TextFieldFormik';
+import { getDocs } from "firebase/firestore";
+import { Form, Formik, FormikHelpers } from "formik";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import { addProject } from "../../context/projectsSlice";
+import { searchWorkers } from "../../firebase/queries";
+import { handleCreateProject } from "../../handlers/handleProject";
+import { createProjectDTO } from "../../model/projects.model";
+import { CREATE_PROJECT_VALIDATION_SCHEMA } from "../../utilities/formValidations";
+import SelectFormik from "../form/SelectFormik";
+import TextFieldFormik from "../form/TextFieldFormik";
 
 const INITIAL_VALUES: createProjectDTO = {
-  name: '',
+  name: "",
   workers: [],
 };
 
@@ -32,15 +32,15 @@ const CreateProjects: React.FunctionComponent = (props) => {
     helpers: FormikHelpers<createProjectDTO>
   ) => {
     const newProject = await handleCreateProject(values);
-    if (typeof newProject === 'string') helpers.setStatus(newProject);
+    if (typeof newProject === "string") helpers.setStatus(newProject);
     else if (newProject) {
       helpers.resetForm();
       dispatch(addProject(newProject));
       Swal.fire({
-        text: 'Se ha creado el proyecto de forma exitosa',
-        icon: 'success',
-        confirmButtonColor: '#31C48D',
-        confirmButtonText: 'Aceptar',
+        text: "Se ha creado el proyecto de forma exitosa",
+        icon: "success",
+        confirmButtonColor: "#31C48D",
+        confirmButtonText: "Aceptar",
       });
     }
   };
@@ -52,15 +52,15 @@ const CreateProjects: React.FunctionComponent = (props) => {
       onSubmit={handleSubmit}
     >
       {({ status }) => (
-        <Form className='h-11/12 md:w-1/3 mx-2 flex flex-col items-center gap-5 border-2 border-secondary p-10 overflow-y-auto'>
-          <TextFieldFormik name='name' placeholder='Project Name' />
+        <Form className="h-11/12 md:w-1/3 mx-2 flex flex-col items-center gap-5 border-2 border-secondary p-10 overflow-y-auto">
+          <TextFieldFormik name="name" placeholder="Project Name" />
           <SelectFormik
-            name='workers'
+            name="workers"
             options={avalaibleWorkers}
-            placeholder='Selecciona a los encargados'
+            placeholder="Selecciona a los encargados"
             renderList
           />
-          <button type='submit' className='btn btn-primary text-white w-full'>
+          <button type="submit" className="btn btn-primary text-white w-full">
             Create
           </button>
           {!!status && <div>{status}</div>}
